97fd60b2090e7f61cf7a81da4b6d5a1bf92fa54e
Flozenfr <flozenfr@gmail.com>
2025-05-22 16:28:14 +0200

    main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,3 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault(); // Empêche le prompt automatique
-  deferredPrompt = e; // Sauvegarde pour l'utiliser plus tard
-  showInstallPopup(); // Fonction personnalisée pour afficher le popup
-});
